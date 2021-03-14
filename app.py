9e4acefe1895eb5b910ac100f4c1036b723fee7c
“[neeraj-cloud9-twitchProj]” <neerajsomani04@gmail.com>
2021-03-14 20:56:15 +0000

    first episod code commiting after sucessful run

--- app.py
+++ app.py
@@ -2,16 +2,10 @@
 
 from aws_cdk import core as cdk
 
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
-from twitch_aws_image_rekognition.twitch_aws_image_rekognition_stack import TwitchAwsImageRekognitionStack
+from twitch_aws_image_rekognition.backend_stack import BackendStack
 
 
 app = cdk.App()
-TwitchAwsImageRekognitionStack(app, "twitch-aws-image-rekognition")
+BackendStack(app, "twitch-aws-image-rekognition", env={'region': 'us-west-2'})
 
 app.synth()
--- setup.py
+++ setup.py
@@ -20,6 +20,25 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.92.0",
+        "aws-cdk.aws_iam==1.92.0",
+        "aws-cdk.aws_sqs==1.92.0",
+        "aws-cdk.aws_sns==1.92.0",
+        "aws-cdk.aws_sns_subscriptions==1.92.0",
+        "aws-cdk.aws_s3==1.92.0",
+        "aws-cdk.aws_events==1.92.0",
+        "aws-cdk.aws_events_targets==1.92.0",
+        "aws-cdk.aws_dynamodb==1.92.0",
+        "aws-cdk.aws_apigateway==1.92.0",
+        "cdk-spa-deploy==1.92.0",
+        "botocore",
+        "boto3",
+        "aws-cdk.aws-lambda==1.92.0",
+        "aws-cdk.aws-lambda-event-sources==1.92.0",
+        "aws_cdk.aws_s3_deployment==1.92.0",
+        "aws_cdk.aws_apigateway==1.92.0",
+        "aws_cdk.aws_cognito==1.92.0",
+        "aws_cdk.aws_s3_notifications==1.92.0",
+
     ],
 
     python_requires=">=3.6",
